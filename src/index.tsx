74916592799daf7adb22c0b202c406487756932a
Will Jackson <wrejcodes@gmail.com>
2017-04-29 15:26:48 -0500

    initial file structure for react components and main entry point. Webpack and typescript configuration files also set up.

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+// here is where we will import the other components we make from  ./components/Name_of_component
+
+
+
+
+//This is where we will render our app
+// for now we don't have any components to render so I'll just leave this commented
+
+// ReactDOM.render(
+// 	<App />, document.getElementById("content") 	
+// );
